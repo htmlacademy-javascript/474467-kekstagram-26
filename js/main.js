ef726741670084c70ee2ddc8c7764261d5778b6d
Kristina <shvindtchristina@gmail.com>
2022-06-23 15:28:55 +0300

    homework

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ function checkStringLength(string, maxLength) {
     return false;
   }
 }
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
